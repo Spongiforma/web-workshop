3a3ca3fec748bfc18d8b5b295b6b570ef50b4226
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2021-07-31 15:04:50 +0800

    Add profile pictures

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -23,12 +23,13 @@
               <v-list-item-title class="text-h5 mb-1">
                 {{ student.name }}
               </v-list-item-title>
-              <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+<!--              <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>-->
             </v-list-item-content>
             <v-list-item-avatar
                 tile
                 size="80"
                 color="grey"
+                datasrc="@/../public/assets/logo.png"
             ></v-list-item-avatar>
           </v-list-item>
           </a>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,32 @@
 <template>
   <v-container>
     <v-card class="card">
-        <v-img class="center profile-img" max-width="200px" src="@/../public/assets/logo.png" ></v-img>
+      <v-menu
+      v-model="menu"
+      nudge-right="20"
+      :close-on-content-click="false"
+      offset-x>
+        <template v-slot:activator="{ on, attrs }">
+          <v-img class="center profile-img" max-width="200px" :src="src"
+                 v-bind="attrs"
+                 v-on="on"
+          ></v-img>
+        </template>
+        <v-card width="400px" class="popover">
+          <v-list>
+            <v-text-field
+              label="Image link"
+              placeholder="Replace profile picture"
+            v-model="imageLink">
+            </v-text-field>
+            <p>You can use <a>catbox.moe</a></p>
+            <v-btn
+                v-on:click="updatePfp">
+              Update
+            </v-btn>
+          </v-list>
+        </v-card>
+      </v-menu>
         <h3> Name: </h3> <p>{{ user.name }}</p>
         <h3> Class: </h3> <p>{{ user.class }}</p>
     </v-card>
@@ -16,6 +41,9 @@ export default Vue.extend({
   data()  {
     return {
       user: {},
+      menu: false,
+      imageLink: "",
+      src: "@/../public/assets/logo.png"
     };
   },
   methods : {
@@ -23,11 +51,34 @@ export default Vue.extend({
       return fetch("/api/student").then(res => res.json()).then(res => {
         return res[0];
       });
+    },
+    updatePfp() {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        fetch(`/api/student/${this.user._id}`,{
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            profilePicture: this.imageLink,
+          })
+        });
     }
   },
   async mounted() {
     this.user = await this.getUser();
     console.log(this.user);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if(this.user.profilePicture) {
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.src = this.user.profilePicture;
+      this.menu=false;
+    }
   }
 
 });
@@ -39,9 +90,13 @@ export default Vue.extend({
 .profile-img {
   margin-left: auto;
   margin-right: auto;
+  cursor: pointer;
 }
 
 .card {
+  padding: 2rem;
+}
+.popover {
   padding: 2rem;
 }
 </style>
